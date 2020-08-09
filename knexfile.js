edf87742304f2e753f3f0b19c5fffc1deecbc426
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-08-09 16:12:46 -0300

    Ajustando as configuraçoes para acesso ao banco postgres

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,11 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'knex_test',
+      user: 'alisson',
+      password: 'inoino'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
-
 };
