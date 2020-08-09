6e0086a33a179bf5ebde96f2f90c40bc2418b5c3
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-08-09 16:30:34 -0300

    Adicionado a primeira rota para mostrar todos os usuarios

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const knex = require('./database')
 
+app.get('/users', (req,res)=> knex('users').then((results)=>res.json(results)))
 
 app.listen(3000, ()=> console.log("Servidor está rodando!")) 
