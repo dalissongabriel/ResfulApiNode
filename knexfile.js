00c7caf0c376cac867e599e09fddddb00861a9b7
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-08-09 16:25:16 -0300

    Adicionando seeds para popular a tabela de usuários

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,9 @@ module.exports = {
     },
     migrations: {
       directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
     }
   }
 };
