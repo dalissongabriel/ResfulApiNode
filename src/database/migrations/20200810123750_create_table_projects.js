409b09bf98dc50a4c10347308e00673f55e44410
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-08-16 21:12:34 -0300

    Trigger para updated_at atualizar com procedure.

--- src/database/migrations/20200810123750_create_table_projects.js
+++ src/database/migrations/20200810123750_create_table_projects.js
@@ -1,3 +1,5 @@
+const { onUpdateTrigger } = require('../../../knexfile')
+
 exports.up = async knex => knex.schema.createTable('projects',(table)=>{
     table.increments('id')
     table.text('title').notNullable()
@@ -7,6 +9,6 @@ exports.up = async knex => knex.schema.createTable('projects',(table)=>{
         .onDelete('CASCADE')
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
-})
+}).then(()=>knex.raw(onUpdateTrigger('projects')))
 
-exports.down = knex => knex.schema.dropTable('projects')
+exports.down = async knex => knex.schema.dropTable('projects')
